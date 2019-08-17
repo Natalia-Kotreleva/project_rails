6e9100ef23a966010a924109f721f3002abc182a
Natalia-Kotreleva <kotreleva92@gmail.com>
2019-08-17 14:22:17 +0300

    add author

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :static_pages do
+    get 'about/author'
+  end
   get 'static_pages/about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
