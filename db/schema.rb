e1f280769caaa3b3cbada664683efe0578526190
Natalia-Kotreleva <kotreleva92@gmail.com>
2019-08-25 19:00:06 +0300

    user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_25_155655) do
+ActiveRecord::Schema.define(version: 2019_08_25_155935) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body"
@@ -30,6 +30,12 @@ ActiveRecord::Schema.define(version: 2019_08_25_155655) do
   create_table "tests", force: :cascade do |t|
     t.string "title"
     t.integer "level"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
