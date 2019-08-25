7351fc1c7e3b383c8e8272d26590054272cae548
Natalia-Kotreleva <kotreleva92@gmail.com>
2019-08-25 19:02:12 +0300

    category

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_25_155935) do
+ActiveRecord::Schema.define(version: 2019_08_25_160141) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body"
     t.integer "question_id"
     t.boolean "correct"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
